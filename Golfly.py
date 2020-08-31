314c6166e6e2644787b8b46348299a42504fd3c2
Jeff <jeff.r.finn@gmail.com>
2020-08-31 21:09:33 +0100

    Read data from csv using pandas

---
+++ Golfly.py
@@ -0,0 +1,31 @@
+import pandas 
+
+print("Welcome to Golfly")
+
+print("Importing Data...")
+
+dtype = {
+    "player_name" : str,
+    "date": str,
+    "tournament": str,
+    "statistic": str,
+    "variable": str,
+    "value": str
+    }
+
+data = pandas.read_csv(
+    "./Data/PGA_Data_Historical.csv", 
+    sep=',',
+    quotechar='"',
+    dtype={
+        "player_name" : str,
+        "tournament": str,
+        "statistic": str,
+        "variable": str,
+        "value": str
+    },
+    parse_dates=['date']) 
+
+print("Data Imported")
+print("# of records: " + str(data.size))
+print(data.head())
