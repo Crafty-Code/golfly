9f66c7c64b6071271049959cc0a8738e3987d548
Jeff <jeff.r.finn@gmail.com>
2020-09-03 22:23:45 +0100

    Build a list of winnings by player and sum totals

--- Golfly.py
+++ Golfly.py
@@ -4,15 +4,6 @@ print("Welcome to Golfly")
 
 print("Importing Data...")
 
-dtype = {
-    "player_name" : str,
-    "date": str,
-    "tournament": str,
-    "statistic": str,
-    "variable": str,
-    "value": str
-    }
-
 data = pandas.read_csv(
     "./Data/PGA_Data_Historical.csv", 
     sep=',',
@@ -29,5 +20,21 @@ data = pandas.read_csv(
 print("Data Imported")
 print("# of records: " + str(data.size))
 
-listOfStats = data.groupby(['statistic'])['variable'].unique() 
-listOfStats.to_json("./Data/statistics.json")
+# listOfStats = data.groupby(['statistic'])['variable'].unique() 
+# listOfStats.to_json("./Data/statistics.json")
+
+listOfWinnings = data.query("statistic == 'Official Money' and variable == 'MONEY'")
+print("List of winnings retrieved")
+
+# clean up values
+listOfWinnings['value'] = listOfWinnings['value'].replace('[\$,]', '', regex=True)
+listOfWinnings['value'] = pandas.to_numeric(listOfWinnings.value, errors='coerce')
+print("Cleaned up values")
+
+# sum all of each players winnings
+output = listOfWinnings.groupby(['player_name'])[['value']].sum()
+
+# sort in descending order
+output = output.sort_values(by=['value'], ascending=False)
+print(output)
+output.to_json("./Data/output.json")
